c71af90849b63a6639457a049b5df2d789869cf2
Michael <liukaiyi54@gmail.com>
2015-09-12 08:10:40 +0800

    add random color, changes on touch

--- Hypnosister/HypnosisterView.m
+++ Hypnosister/HypnosisterView.m
@@ -8,6 +8,12 @@
 
 #import "HypnosisterView.h"
 
+@interface HypnosisterView()
+
+@property (nonatomic, strong) UIColor *circleColor;
+
+@end
+
 @implementation HypnosisterView
 
 - (id)initWithFrame:(CGRect)frame
@@ -15,6 +21,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
+        self.circleColor = [UIColor lightGrayColor];
     }
     return self;
 }
@@ -38,7 +45,7 @@
     }
     
     path.lineWidth = 10;
-    [[UIColor lightGrayColor] setStroke];
+    [self.circleColor setStroke];
     [path stroke];
     
     UIImage *image = [UIImage imageNamed:@"myself"];
@@ -50,5 +57,21 @@
     
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@ was touched", self);
+    
+    float red = (arc4random() % 100) / 100.0;
+    float green = (arc4random() % 100) / 100.0;
+    float blue = (arc4random() % 100) / 100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    
+    self.circleColor = randomColor;
+}
+
+- (void)setCircleColor:(UIColor *)circleColor {
+    _circleColor = circleColor;
+    [self setNeedsDisplay];
+}
 
 @end
